716e1a16b2be839e8d3877d0c2c7d84781e817c2
Дмитрий Кассин <kassindima1@gmail.com>
2022-07-04 10:20:12 +0300

    \\\

--- ParentalControl/ParentalControl/Program.cs
+++ ParentalControl/ParentalControl/Program.cs
@@ -13,42 +13,42 @@ namespace ParentalControl
     {
         static void Main(string[] args)
         {
-            Task.Factory.StartNew(() =>
-            {
-                while (true)
-                {
-                    var procceses = from proc in Process.GetProcesses()
-                                    where proc.ProcessName == "chrome.exe"
-                                    select proc;
-                    try
-                    {
-                        foreach (var p in procceses)
-                            p.Kill();
-                    }
-                    catch (Exception)
-                    {
-                    }
-                    Thread.Sleep(500);
-                }
-            });
-            Console.ReadLine();
-
-            //DateTime date1 = DateTime.Now;
-            //DateTime date2 = new DateTime(2022, 07, 04, 8, 4, 6);
-            //int result = DateTime.Compare(date1, date2);
-
-            //if (date1.Hour < 9)
+            //Task.Factory.StartNew(() =>
             //{
-            //    Process[] processes = Process.GetProcessesByName("");
-            //    foreach (Process process in processes)
+            //    while (true)
             //    {
-            //        process.Kill();
+            //        var procceses = from proc in Process.GetProcesses()
+            //                        where proc.ProcessName == "chrome"
+            //                        select proc;
+            //        try
+            //        {
+            //            foreach (var p in procceses)
+            //                p.Kill();
+            //        }
+            //        catch (Exception)
+            //        {
+            //        }
+            //        Thread.Sleep(500);
             //    }
-            //}
-            //else
-            //{
-            //    Environment.Exit(0);
-            //}
+            //});
+            //Console.ReadLine();
+
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = new DateTime(2022, 07, 04, 13, 0, 0);
+            int result = DateTime.Compare(date1, date2);
+
+            if (date1.Hour < 13)
+            {
+                Process[] processes = Process.GetProcessesByName("chrome");
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
 
         }
     }
